65fa7fc14fe601e0942a581ac729391438ed7467
Zayn <140482441+Zayn1119@users.noreply.github.com>
2025-09-16 13:25:15 +1000

    Add files via upload

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(135deg,rgb(167, 117, 75) 0%,rgb(231, 109, 38) 100%)',
+      }
+    },
+  },
   plugins: [],
 }
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5175 },
+  server: { 
+    port: 5173,
+    host: '0.0.0.0'
+  },
   // ★ Safari向け：依存を事前バンドルして再エクスポート問題を回避
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-router-dom', 'axios', 'jwt-decode'],
